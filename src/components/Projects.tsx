8414dbbe7752d93bc4a34e63704805e57fb157a4
Shivam Agrawal <en23ca503169@medicaps.ac.in>
2026-01-11 02:03:16 +0530

    Add files via upload

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,10 +17,6 @@ import {
   EffectCoverflow,
 } from "swiper/modules";
 import type { Swiper as SwiperType } from "swiper";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/effect-coverflow";
 
 const projects = [
   {
